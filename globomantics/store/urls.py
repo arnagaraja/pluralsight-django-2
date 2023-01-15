2e7e551c118d40866568e3f3f0463b38928cd43b
Arpun Nagaraja <arnagaraja@gmail.com>
2023-01-14 20:09:53 -0600

    Class-based views; View, TemplateView, ListView

--- globomantics/store/urls.py
+++ globomantics/store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
 from . import views
+from .views import ElectronicsView, ElectronicsView2, ElectronicsView3
 
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^\d+', views.detail, name='detail'),
-    re_path(r'^electronics', views.electronics, name='electronics'),
+    #re_path(r'^electronics', views.electronics, name='electronics'),
+    re_path(r'^electronics', ElectronicsView3.as_view(), name='electronics'),
 ]
--- globomantics/store/views.py
+++ globomantics/store/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.cache import cache_page
 from django.core.paginator import Paginator
+from django.views.generic import View, TemplateView, ListView
 
 # Create your views here.
 def index(request):
@@ -12,12 +13,24 @@ def index(request):
 def detail(request):
     return HttpResponse("store front detail pages coming soon")
 
-#@csrf_exempt
-@require_http_methods(["GET"])
-@cache_page(900)
-def electronics(request):
-    items = ("item 1", "item 2", "item 3", "item 4")
-    if request.method == 'GET':
+# #@csrf_exempt
+# @require_http_methods(["GET"])
+# @cache_page(900)
+# def electronics(request):
+#     items = ("item 1", "item 2", "item 3", "item 4")
+#     if request.method == 'GET':
+#         paginator = Paginator(items, 2)
+#         pages = request.GET.get('page', 1)
+#         try:
+#             items = paginator.page(pages)
+#         except PageNotAnInteger:
+#             items = paginator.page(1)
+#         return render(request, 'store/list.html', {'items': items})
+#     return HttpResponse("store front electronics pages coming soon")
+
+class ElectronicsView(View):
+    def get(self, request):
+        items = ("item 1", "item 2", "item 3", "item 4", "item 5", "item 6")
         paginator = Paginator(items, 2)
         pages = request.GET.get('page', 1)
         try:
@@ -25,4 +38,16 @@ def electronics(request):
         except PageNotAnInteger:
             items = paginator.page(1)
         return render(request, 'store/list.html', {'items': items})
-    return HttpResponse("store front electronics pages coming soon")
+
+class ElectronicsView2(TemplateView):
+    template_name = 'store/list.html'
+    def get_context_data(self, **kwargs):
+        items = ("item 1", "item 2", "item 3", "item 4", "item 5", "item 6")
+        context = {'items': items}
+        return context
+
+class ElectronicsView3(ListView):
+    template_name = 'store/list.html'
+    queryset = ("item 1", "item 2", "item 3", "item 4", "item 5", "item 6")
+    context_object_name = 'items'
+    paginate_by = 2
